9d1c78f138cf2318b7b3b28667436752d8163c03
vijayhardaha <vijayhardaha@gmail.com>
2021-05-17 22:29:15 +0530

    Update css

--- components/meta.js
+++ components/meta.js
@@ -76,9 +76,8 @@ const Meta = () => {
 
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link
-				href="https://fonts.googleapis.com/css2?family=Federo&family=Montserrat:wght@300;400;500;600;700;900&display=swap"
+				href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600&family=Teko:wght@700&display=swap"
 				rel="stylesheet"
-				crossOrigin="anonymous"
 			/>
 
 			<meta
